bdc7fa6ca3d7dc4cc51b4914bb2e753a30b2f3d1
KuzAnton8335 <ant2783@gmail.com>
2025-02-25 16:03:54 +0300

    blog html

--- src/app/main.js
+++ src/app/main.js
@@ -2,8 +2,11 @@ import "normalize.css";
 import "../styles/main.scss";
 import Header from "./Header.js";
 import TabsCollection from "./Tabs.js";
+import VideoPlayerCollection from "./VideoPlayer.js";
 
 // логика появления burger menu в header
 new Header();
 // логика для TabsCollection появления табов на странице
 new TabsCollection();
+// логика для видео плеера
+new VideoPlayerCollection();
